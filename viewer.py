5bf4eb95a5ffe12b05a928ced02246343096666a
Ercan Sezdi <45130158+ercansezdi@users.noreply.github.com>
2024-07-06 10:48:00 +0300

    Create viewer.py

---
+++ viewer.py
@@ -0,0 +1,83 @@
+# import pyjsonviewer 
+
+# aa = {202598: {'name': '★★ ULUSAL ★★', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TURK_BAYRAGI_ORIGI.png', 'num': 2}, 27: {'name': 'TR • TRT 4K', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/trt-4k-tr.png', 'num': 3}, 99796: {'name': 'TR • TRT 1 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/trt-1-tr.png', 'num': 4}, 6363: {'name': 'TR • TRT 1 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/trt-1-tr.png', 'num': 5}, 101506: {'name': 'TR • TRT 2 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/trt-2-tr.png', 'num': 6}, 6364: {'name': 'TR • TRT 2 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/trt-2-tr.png', 'num': 7}, 100017: {'name': 'TR • KANAL D HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/kanal-d-tr.png', 'num': 8}, 6374: {'name': 'TR • KANAL D FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/kanal-d-tr.png', 'num': 9}, 5: {'name': 'TR • TV8 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/tv8-tr.png', 'num': 10}, 6370: {'name': 'TR • TV8 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/tv8-tr.png', 'num': 11}, 6373: {'name': 'TR • STARTV HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/star-tv-tr.png', 'num': 12}, 7: {'name': 'TR • STAR TV FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/star-tv-tr.png', 'num': 13}, 9: {'name': 'TR • SHOW TV HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/show-tr.png', 'num': 14}, 6372: {'name': 'TR • SHOWTV FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/show-tr.png', 'num': 15}, 11: {'name': 'TR • ATV HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/atv-tr.png', 'num': 16}, 6366: {'name': 'TR • ATV FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/atv-tr.png', 'num': 17}, 13: {'name': 'TR • NOWTV HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/now-tr.png', 'num': 18}, 6367: {'name': 'TR • NOWTV FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/now-tr.png', 'num': 19}, 100358: {'name': 'TR • A2 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/a2-tr.png', 'num': 20}, 6365: {'name': 'TR • A2 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/a2-tr.png', 'num': 21}, 6375: {'name': 'TR • TEVE 2 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/teve2-tr.png', 'num': 22}, 101504: {'name': 'TR • TEVE 2 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/teve2-tr.png', 'num': 23}, 101505: {'name': 'TR • BEYAZTV HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/beyaz-tv-tr.png', 'num': 24}, 6368: {'name': 'TR • BEYAZTV FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/beyaz-tv-tr.png', 'num': 25}, 6369: {'name': 'TR • KANAL7 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/kanal-7-tr.png', 'num': 26}, 101507: {'name': 'TR • KANAL7 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/kanal-7-tr.png', 'num': 27}, 101508: {'name': 'TR • TV8.5 HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/tv85-tr.png', 'num': 28}, 6371: {'name': 'TR • TV8.5 FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/tv85-tr.png', 'num': 29}, 21: {'name': 'TR • EURO D HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/euro-d-tr.png', 'num': 30}, 147546: {'name': 'TR • EUROSTAR HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/euro-star-tr.png', 'num': 31}, 147547: {'name': 'TR • ATV AVRUPA HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/atv-avrupa-tr.png', 'num': 32}, 147548: {'name': 'TR • SHOW TURK', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/show-tr.png', 'num': 33}, 6329: {'name': 'TR • 360 TV FHD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/360-tr.png', 'num': 451}, 219228: {'name': 'TR • TRT TÜRK HD', 'stream_icon': 'http://logos.fixserver.xyz/Logo/TR/TRORIGINALLOGO/trt-turk-tr.png', 'num': 594}}
+
+# pyjsonviewer.view_data(aa)
+
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import  *
+from PyQt5.QtGui import *
+import json
+
+
+
+
+
+
+class JsonViewer(QDialog):
+    def __init__(self, parent=None):
+        super(JsonViewer, self).__init__(parent)
+        self.setWindowTitle("Json Viewer")
+        #add maximize minimize button
+        self.setMinimumSize(800, 600)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.tree = QTreeView()
+        self.layout.addWidget(self.tree)
+        self.add_new = QPushButton("Add New")
+        self.layout.addWidget(self.add_new)
+        self.add_new.clicked.connect(self.addNew)
+        self.model = QStandardItemModel()
+        self.tree.setModel(self.model)
+        self.clearJson()
+        self.addNew()
+    def addNew(self):
+        self.add_new.hide()
+        self.tree.hide()
+        self.clearJson()
+        self.plainText = QPlainTextEdit()
+        self.layout.addWidget(self.plainText)
+        self.plainText.setPlaceholderText("Enter Json Here")
+        self.add = QPushButton("Show Json")
+        self.layout.addWidget(self.add)
+        self.add.clicked.connect(self.addJson)
+    def addJson(self):
+        
+        text = self.plainText.toPlainText()
+        try:
+            textx = dict(eval(text))
+            self.loadJson(textx)
+            self.add_new.show()
+            self.tree.show()
+            self.plainText.hide()
+            self.add.hide()
+        except:
+            QMessageBox.critical(self, "Error", "Invalid Json")
+    def loadJson(self, data):
+        def parse(data, parent):
+            if isinstance(data, dict):
+                for key, value in data.items():
+                    item = QStandardItem(str(key))
+                    parent.appendRow(item)
+                    parse(value, item)
+            elif isinstance(data, list):
+                for value in data:
+                    item = QStandardItem("")
+                    parent.appendRow(item)
+                    parse(value, item)
+            else:
+                item = QStandardItem(str(data))
+                parent.appendRow(item)
+
+        parse(data, self.model.invisibleRootItem())
+    def clearJson(self):
+        self.model.clear()
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    viewer = JsonViewer()
+    viewer.show()
+    sys.exit(app.exec_())
